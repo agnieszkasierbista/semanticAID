5b95c5e5bbd06ce486c8773c919fd6109e12edd9
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-04-25 21:42:33 +0200

    Add text input

--- App.tsx
+++ App.tsx
@@ -1,17 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput style={styles.input}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "lightgray",
+    borderWidth: 2,
+    borderStyle: "dashed",
+    borderColor: "black",
+    borderRadius: 5,
+    width: 150,
+    aspectRatio: 2,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
