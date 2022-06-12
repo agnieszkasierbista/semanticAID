65a177f4ba86bf2e57b14a536486862640e32d95
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-06-12 21:10:59 +0200

    Refactor

--- notes.md
+++ notes.md
@@ -1,2 +1,7 @@
 1. struktura danych:
  - uwzględnić strukturę danych aby wyświetlić pierwsze pytanie (czyli wychodzimy od danych w celu utworzenia komponentu)
+
+A {
+q: "?",
+el: "nav"
+}
--- questions.ts
+++ questions.ts
@@ -1,1 +1,34 @@
-export const questions = ["abc", "def", "ghi", "jkl"];
+export const questions =
+    {
+        q: "Is it a major navigation block?",
+        yes: "nav",
+        no: {
+            q: "Does it make sense on its own?",
+            yes: "article",
+            no: {
+                q: "Is it required to understand the current content?",
+                yes: {
+                    q: "Could you move it to an appendix?",
+                    yes: "figure",
+                    no: {
+                        q: "Is it logical to add a heading?",
+                        yes: "section",
+                        no: {
+                            q: "Is it introductory content?",
+                            yes: "header",
+                            no: {
+                                q: "Does the content contain information about an author, related links, or legal information?",
+                                yes: "footer",
+                                no: {
+                                    q: "Does it have any semantics?",
+                                    yes: "p", //or address, blockquote, pre....
+                                    no: "div"
+                                }
+                            }
+                        }
+                    }
+                },
+                no: "aside"
+            }
+        }
+    };
